d4cc7d091cad93bacdd58f789741853ffb786335
liuzifeng <liuzifeng@meituan.com>
2021-05-31 22:06:07 +0800

    format

--- Lexer.ts
+++ Lexer.ts
@@ -5,108 +5,103 @@ import { Token, NumToken, StrToken, IdToken } from './Token'
 
 const commentRegExp = '//.*'
 const numRegExp = '\\d+'
-const strRegExp = '"(\\"|\\\\|\\n|[^\"])*"'
-const idRegExp = '[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||' +
-	 '[^A-Za-z0-9\s]'
+const strRegExp = '"(\\"|\\\\|\\n|[^"])*"'
+const idRegExp = '[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||' + '[^A-Za-z0-9s]'
 // java \p{Punct} => Punctuation: One of !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 // https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
 const regexPattern = `\\s*((${commentRegExp})|(${numRegExp})|(${strRegExp})|(${idRegExp}))?`
 
 class Lexer {
+    // 存放每行读取的token
+    private queue: Token[] = []
+    private hasMore: boolean
+    private reader
 
-	// 存放每行读取的token
-	private queue: Token[] = []
-	private hasMore: boolean
-	private reader
+    constructor(filePath) {
+        this.hasMore = true
+        this.reader = readline.createInterface({
+            input: fs.createReadStream(filePath),
+            crlfDelay: Infinity,
+        })
+        // Note: we use the crlfDelay option to recognize all instances of CR LF ('\r\n') in input.txt as a single line break.
+    }
 
-	constructor (filePath) {
-		this.hasMore = true
-		this.reader = readline.createInterface({
-			input: fs.createReadStream(filePath),
-			crlfDelay: Infinity
-		})
-		// Note: we use the crlfDelay option to recognize all instances of CR LF ('\r\n') in input.txt as a single line break.
-	}
+    public read(): Token {
+        try {
+            if (this.fillQueue(0)) {
+                return this.queue.shift()
+            }
+        } catch (e) {}
 
-	public read (): Token {
-		try {
-			if (this.fillQueue(0)) {
-				return this.queue.shift()
-			}
-		} catch (e) {
+        return Token.EOF
+    }
 
-		}
+    /**
+     * 预读, 返回read 方法即将返回的单词之后的第i 个单词
+     *
+     * @param i 单词位置
+     * @return 返回一个单词
+     */
+    public peek(i): Token {
+        if (this.fillQueue(i)) {
+            return this.queue[i]
+        }
 
-		return Token.EOF;
-	}
+        return Token.EOF
+    }
 
-	/**
-	 * 预读, 返回read 方法即将返回的单词之后的第i 个单词
-	 *
-	 * @param i 单词位置
-	 * @return 返回一个单词
-	 */
-	public peek (i): Token {
-		if (this.fillQueue(i)) {
-			return this.queue[i]
-		}
+    private fillQueue(i): boolean {
+        try {
+            while (i >= this.queue.length) {
+                if (this.hasMore) {
+                    this.readLine('', 0)
+                } else {
+                    return false
+                }
+            }
+        } catch (e) {}
+        return true
+    }
 
-		return Token.EOF
-	}
+    public process() {
+        let lineNumber = 1
+        this.reader.on('line', (line) => {
+            console.log('Line from file:', line)
+            lexer.readLine(line, lineNumber++)
+        })
+    }
 
-	private fillQueue(i): boolean {
-		try {
-			while (i >= this.queue.length) {
-				if (this.hasMore) {
-					this.readLine('', 0)
-				} else {
-					return false
-				}
-			}
-		} catch (e) {
+    private readLine(line: string, lineNumber: number) {
+        let match
+        const pattern = new RegExp(regexPattern, 'g')
+        while ((match = pattern.exec(line))) {
+            // console.log('Found ' + myArray[1])
+            this.addToken(lineNumber, match)
+            if (pattern.lastIndex >= line.length) {
+                break
+            }
+        }
+        this.queue.push(new IdToken(lineNumber, Token.EOL))
+    }
 
-		}
-		return true
-	}
-
-	public process () {
-		let lineNumber = 1
-		this.reader.on('line', (line) => {
-			console.log('Line from file:', line)
-			lexer.readLine(line, lineNumber++)
-		})
-	}
-
-	private readLine (line: string, lineNumber: number) {
-		let match
-		const pattern = new RegExp(regexPattern, 'g')
-		while (match = pattern.exec(line)) {
-  			// console.log('Found ' + myArray[1])
-			this.addToken(lineNumber, match)
-			if (pattern.lastIndex >= line.length) {
-				break
-			}
-		}
-		this.queue.push(new IdToken(lineNumber, Token.EOL))
-	}
-
-	private addToken(lineNo, match) {
-		const m = match[1]
-		if (m?.trim()) {    // if not a space
-			if (!match[2]) {     // if not a comment
-				let token = null
-				if (match[3]) {
-					token = new NumToken(lineNo, Number(m))
-				} else if (match[4]) {
-					token = new StrToken(lineNo, m)
-				} else {
-					token = new IdToken(lineNo, m)
-				}
-				this.queue.push(token)
-			}
-		}
-	}
-
+    private addToken(lineNumber: number, match: Array<string>) {
+        const m = match[1]
+        if (m?.trim()) {
+            // if not a space
+            if (!match[2]) {
+                // if not a comment
+                let token = null
+                if (match[3]) {
+                    token = new NumToken(lineNumber, Number(m))
+                } else if (match[4]) {
+                    token = new StrToken(lineNumber, m)
+                } else {
+                    token = new IdToken(lineNumber, m)
+                }
+                this.queue.push(token)
+            }
+        }
+    }
 }
 
 // test
--- Token.ts
+++ Token.ts
@@ -1,89 +1,88 @@
-enum Type {
-	unknown = 0,
-	number = 1,
-	string = 2,
-	identifier = 3
+export enum Type {
+    unknown = 0,
+    number = 1,
+    string = 2,
+    identifier = 3,
 }
 
 export class Token {
-	public static EOF = new Token(-1)
+    public static EOF = new Token(-1)
 
-	// 换行
-	public static EOL = '\n'
+    // 换行
+    public static EOL = '\n'
 
-	private readonly lineNumber = 0
+    private readonly lineNumber = 0
 
-	constructor (lineNumber) {
-		this.lineNumber = lineNumber
-	}
+    constructor(lineNumber) {
+        this.lineNumber = lineNumber
+    }
 
-	get type (): Type {
-		return Type.unknown
-	}
+    get type(): Type {
+        return Type.unknown
+    }
 
-	// 数字字面量
-	public getNumber (): number {
-		throw new Error('not number token')
-	}
+    // 数字字面量
+    public getNumber(): number {
+        throw new Error('not number token')
+    }
 
-	// 节点文本
-	public getText (): string {
-		return ''
-	}
+    // 节点文本
+    public getText(): string {
+        return ''
+    }
 }
 
 export class NumToken extends Token {
-	private value = 0
+    private value = 0
 
-	constructor (lineNumber, value) {
-		super(lineNumber)
-		this.value = value
-	}
+    constructor(lineNumber, value) {
+        super(lineNumber)
+        this.value = value
+    }
 
-	public get type () {
-		return Type.number
-	}
+    public get type() {
+        return Type.number
+    }
 
-	public getText () {
-		return String(this.value)
-	}
+    public getText() {
+        return String(this.value)
+    }
 
-	public getNumber () {
-		return this.value
-	}
+    public getNumber() {
+        return this.value
+    }
 }
 
 export class IdToken extends Token {
-	private name = ''
+    private name = ''
 
-	constructor (lineNumber, name) {
-		super(lineNumber)
-		this.name = name
-	}
+    constructor(lineNumber, name) {
+        super(lineNumber)
+        this.name = name
+    }
 
-	public get type () {
-		return Type.identifier
-	}
+    public get type() {
+        return Type.identifier
+    }
 
-	public getText () {
-		return this.name
-	}
+    public getText() {
+        return this.name
+    }
 }
 
 export class StrToken extends Token {
-	private value = ''
+    private value = ''
 
-	constructor (lineNumber, value) {
-		super(lineNumber)
-		this.value = value
-	}
+    constructor(lineNumber, value) {
+        super(lineNumber)
+        this.value = value
+    }
 
-	public get type () {
-		return Type.string
-	}
+    public get type() {
+        return Type.string
+    }
 
-	public getText () {
-		return this.value
-	}
-
+    public getText() {
+        return this.value
+    }
 }
