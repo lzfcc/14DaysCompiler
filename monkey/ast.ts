57c960e17b5d1f889fcdc0fe677b1a2fc253a5b3
zifengliu <zifengliu@microsoft.com>
2021-12-12 10:42:35 +0800

    prefix and infix

--- monkey/ast.ts
+++ monkey/ast.ts
@@ -1,9 +1,9 @@
-import { ASTLeaf } from '../ast/ASTree'
 import * as token from './token'
 
 interface Node {
     // only for debugging
     tokenLiteral(): string
+    string(): string
 }
 
 // a statment doesn't generate a value
@@ -16,7 +16,11 @@ export interface Expression extends Node {
     expressionNode()
 }
 
-export class Program {
+export class Program implements Statement {
+    statementNode() {}
+    string(): string {
+        return this.statements.map((stmt) => stmt.string()).join()
+    }
     statements: Statement[] = []
     tokenLiteral() {
         if (this.statements.length > 0) {
@@ -37,25 +41,31 @@ export class LetStatement implements Statement {
     token: token.Token
     name: Identifier
     value: Expression
-    constructor (token: token.Token) {
+    constructor(token: token.Token) {
         this.token = token
     }
     statementNode() {}
     tokenLiteral() {
         return this.token.literal // token.LET
     }
+    string(): string {
+        return `${this.tokenLiteral()} ${this.name.string()} = ${this.value.string()};`
+    }
 }
 
 export class Identifier implements Expression {
     token: token.Token
     value: string
-    constructor (token: token.Token, value: string) {
+    constructor(token: token.Token, value: string) {
         this.token = token
         this.value = value
     }
     expressionNode() {}
     tokenLiteral() {
         return this.token.literal // token.IDENT
+    }
+    string(): string {
+        return this.tokenLiteral()
     }
 }
 
@@ -66,7 +76,80 @@ export class ReturnStatement implements Statement {
     }
     token: token.Token
     returnValue: Expression
-    constructor (token: token.Token) {
+    constructor(token: token.Token) {
         this.token = token
+    }
+    string(): string {
+        return `${this.tokenLiteral()} ${this.returnValue.string()};`
+    }
+}
+
+export class ExpressionStatement implements Statement {
+    statementNode() {}
+    tokenLiteral(): string {
+        return this.token.literal
+    }
+    token: token.Token // the first token of the expression
+    expression: Expression
+    constructor(token: token.Token, expr: Expression) {
+        this.token = token
+        this.expression = expr
+    }
+    string(): string {
+        return `${this.expression.string()}`
+    }
+}
+
+export class IntegerLiteral implements Expression {
+    token: token.Token
+    value: number
+    constructor(token: token.Token, val: number) {
+        this.token = token
+        this.value = val
+    }
+    expressionNode() {}
+    tokenLiteral(): string {
+        return this.token.literal
+    }
+    string(): string {
+        return this.token.literal
+    }
+}
+
+export class PrefixExpression implements Expression {
+    expressionNode() {
+    }
+    tokenLiteral(): string {
+        return this.token.literal
+    }
+    string(): string {
+        return `(${this.operator}${this.right.string()})`
+    }
+    token: token.Token
+    operator: string
+    right: Expression
+    constructor(token: token.Token, opr: string) {
+        this.token = token
+        this.operator = opr
+    }
+}
+
+export class InfixExpression implements Expression {
+    expressionNode() {
+    }
+    tokenLiteral(): string {
+        return this.token.literal
+    }
+    string(): string {
+        return `(${this.left.string()}${this.operator}${this.right.string()})`
+    }
+    token: token.Token
+    operator: string
+    left: Expression
+    right: Expression
+    constructor(token: token.Token, opr: string, left: Expression) {
+        this.token = token
+        this.operator = opr
+        this.left = left
     }
 }
--- monkey/parser.ts
+++ monkey/parser.ts
@@ -1,12 +1,70 @@
 import * as token from './token'
 import * as lexer from './lexer'
 import * as ast from './ast'
-import Lexer from '../ast/Lexer'
+
+type PrefixParseFn = () => ast.Expression
+// expr: left expression
+type InfixParseFn = (expr: ast.Expression) => ast.Expression
+
+enum OP_ORDER {
+    LOWEST = 0,
+    EQUALS,
+    LESSGREATER,
+    SUM,
+    PRODUCT,
+    PREFIX, // !x or -x
+    CALL // myfunction(x)
+}
+
+const precedences = {
+    [token.EQ]: OP_ORDER.EQUALS,
+    [token.NOT_EQ]: OP_ORDER.EQUALS,
+    [token.LT]: OP_ORDER.LESSGREATER,
+    [token.GT]: OP_ORDER.LESSGREATER,
+    [token.PLUS]: OP_ORDER.SUM,
+    [token.MINUS]: OP_ORDER.SUM,
+    [token.SLASH]: OP_ORDER.PRODUCT,
+    [token.ASTERISK]: OP_ORDER.PRODUCT,
+}
 
 class Parser {
     l: lexer.Lexer
     curToken: token.Token
     peekToken: token.Token
+
+    prefixParseFns: { [token: token.TokenType]: PrefixParseFn } = {}
+    infixParseFns: { [token: token.TokenType]: InfixParseFn } = {}
+
+    // use arrow function to avoid JS "this" problem!
+    private parseIdentifier = () => {
+        return new ast.Identifier(this.curToken, this.curToken.literal)
+    }
+    private parseIntegerLiteral = () => {
+        const val = Number(this.curToken.literal)
+        const lit = new ast.IntegerLiteral(this.curToken, val)
+        // if (Number.isNaN(val)) {
+        //     msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+        //     p.errors = append(p.errors, msg)
+        //     return nil
+        // }
+        return lit
+    }
+    private parsePrefixExpression = () => {
+        const expr = new ast.PrefixExpression(
+            this.curToken,
+            this.curToken.literal
+        )
+        this.nextToken()
+        expr.right = this.parseExpression(OP_ORDER.PREFIX)
+        return expr
+    }
+    private parseInfixExpression = (left: ast.Expression) => {
+        const expr = new ast.InfixExpression(this.curToken, this.curToken.literal, left)
+        const prec = this.curPrecedence()
+        this.nextToken()
+        expr.right = this.parseExpression(prec)
+        return expr
+    }
 
     constructor(l: lexer.Lexer) {
         this.l = l
@@ -14,14 +72,30 @@ class Parser {
         // Read two tokens, so curToken and peekToken are both set
         this.nextToken()
         this.nextToken()
+
+        // prefix expressions
+        this.registerPrefix(token.IDENT, this.parseIdentifier)
+        this.registerPrefix(token.INT, this.parseIntegerLiteral)
+        this.registerPrefix(token.BANG, this.parsePrefixExpression)
+        this.registerPrefix(token.MINUS, this.parsePrefixExpression)
+
+        // infix expressions
+        this.registerInfix(token.PLUS, this.parseInfixExpression)
+        this.registerInfix(token.MINUS, this.parseInfixExpression) // '-' can also be in an infix expression
+        this.registerInfix(token.SLASH, this.parseInfixExpression)
+        this.registerInfix(token.ASTERISK, this.parseInfixExpression)
+        this.registerInfix(token.EQ, this.parseInfixExpression)
+        this.registerInfix(token.NOT_EQ, this.parseInfixExpression)
+        this.registerInfix(token.LT, this.parseInfixExpression)
+        this.registerInfix(token.GT, this.parseInfixExpression)
     }
 
-    nextToken() {
+    private nextToken() {
         this.curToken = this.peekToken
         this.peekToken = this.l.nextToken()
     }
 
-    ParseProgram(): ast.Program {
+    parseProgram(): ast.Program {
         const program = new ast.Program()
 
         while (this.curToken.type !== token.EOF) {
@@ -35,18 +109,18 @@ class Parser {
         return program
     }
 
-    parseStatement(): ast.Statement {
+    private parseStatement(): ast.Statement {
         switch (this.curToken.type) {
             case token.LET:
                 return this.parseLetStatement()
             case token.RETURN:
                 return this.parseReturnStatement()
             default:
-                return null
+                return this.parseExpressionStatement()
         }
     }
 
-    parseLetStatement(): ast.LetStatement {
+    private parseLetStatement(): ast.LetStatement {
         const stmt = new ast.LetStatement(this.curToken)
 
         if (!this.expectPeek(token.IDENT)) {
@@ -68,7 +142,7 @@ class Parser {
         return stmt
     }
 
-    parseReturnStatement(): ast.Statement {
+    private parseReturnStatement(): ast.Statement {
         const stmt = new ast.ReturnStatement(this.curToken)
 
         this.nextToken()
@@ -82,21 +156,78 @@ class Parser {
         return stmt
     }
 
-    curTokenIs(t: token.TokenType): boolean {
+    private parseExpressionStatement(): ast.Statement {
+        const stmt = new ast.ExpressionStatement(
+            this.curToken,
+            this.parseExpression(OP_ORDER.LOWEST)
+        )
+        if (this.peekTokenIs(token.SEMICOLON)) {
+            this.nextToken()
+        }
+        return stmt
+    }
+
+    // func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+    //     msg := fmt.Sprintf("no prefix parse function for %s found", t)
+    //     p.errors = append(p.errors, msg)
+    // }
+
+    /**
+     * This is the core of Parser
+     * @param precedence: OP_ORDER
+     * @returns ast.Expression
+     */
+    private parseExpression(precedence: OP_ORDER): ast.Expression {
+        const prefix = this.prefixParseFns[this.curToken.type]
+        if (!prefix) {
+            // p.noPrefixParseFnError(p.curToken.Type)
+            return null
+        }
+
+        let leftExpr = prefix()
+
+        while (!this.peekTokenIs(token.SEMICOLON) && precedence < this.peekPrecedence()) {
+            const infix = this.infixParseFns[this.peekToken.type]
+            if (!infix) {
+                return leftExpr
+            }
+            this.nextToken()
+            leftExpr = infix(leftExpr)
+        }
+        return leftExpr
+    }
+
+    private curTokenIs(t: token.TokenType): boolean {
         return this.curToken.type == t
     }
 
-    peekTokenIs(t: token.TokenType): boolean {
+    private peekTokenIs(t: token.TokenType): boolean {
         return this.peekToken.type == t
     }
 
-    expectPeek(t: token.TokenType): boolean {
+    private expectPeek(t: token.TokenType): boolean {
         if (this.peekTokenIs(t)) {
             this.nextToken()
             return true
         } else {
             return false
         }
+    }
+
+    private registerPrefix(tokenType: token.TokenType, fn: PrefixParseFn) {
+        this.prefixParseFns[tokenType] = fn
+    }
+
+    private registerInfix(tokenType: token.TokenType, fn: InfixParseFn) {
+        this.infixParseFns[tokenType] = fn
+    }
+
+    peekPrecedence(): OP_ORDER {
+        return precedences[this.peekToken.type] || OP_ORDER.LOWEST
+    }
+    
+    curPrecedence(): OP_ORDER {
+        return precedences[this.curToken.type] || OP_ORDER.LOWEST
     }
 }
 
@@ -108,7 +239,7 @@ let foobar = 838383;
 `
     const l = new lexer.Lexer(input)
     const p = new Parser(l)
-    const program = p.ParseProgram()
+    const program = p.parseProgram()
 
     if (!program) {
         console.error('ParseProgram() returned nil')
@@ -167,7 +298,7 @@ function TestReturnStatement() {
     const l = new lexer.Lexer(input)
     const p = new Parser(l)
 
-    const program = p.ParseProgram()
+    const program = p.parseProgram()
 
     if (program.statements.length !== 3) {
         console.error(
@@ -188,4 +319,56 @@ function TestReturnStatement() {
     }
 }
 
-TestReturnStatement()
+// TestReturnStatement()
+
+function ParserGeneralTest(scenario, input: string | string[]) {
+    const tester = (input) => {
+        const l = new lexer.Lexer(input)
+        const p = new Parser(l)
+        const program = p.parseProgram()
+
+        console.log(`scenario: ${scenario}`)
+        console.log(program)
+        console.log(program.string())
+    }
+
+    if (Array.isArray(input)) {
+        input.forEach((i) => tester(i))
+    } else {
+        tester(input)
+    }
+}
+
+// ParserGeneralTest('IdentifierExpression', 'foobar;')
+
+// ParserGeneralTest('IntegerLiteralExpression', '2021;')
+
+// ParserGeneralTest('PrefixExpressions', [
+//     '!foobar',
+//     '-20',
+//     '!true',
+// ])
+
+// ParserGeneralTest('InfixExpressions', [
+//     '5 + 5;',
+//     '5 - 5;',
+//     '5 * 5;',
+//     '5 / 5;',
+//     '5 > 5;',
+//     '5 < 5;',
+//     '5 == 5;',
+//     '5 != 5;'
+// ])
+
+ParserGeneralTest('InfixExpressions', [
+    '-a + b;',
+    '!-a;',
+    'a + b - c;',
+    '4 * 5 / 2; 1 + 2',
+    '2 + 1 * 3 + 5',
+    '5 > 5 == 2 < 3;',
+    '5 < 5 != 4 > 2;',
+    '5 * 2 == 5 + 5',
+    '3 > 2 == 4 == 0', // ((3 > 2) == 4) == 0 => true
+    '0 * 1 == 1 > 1', // (0 * 1) == (1 > 1) => true
+])
